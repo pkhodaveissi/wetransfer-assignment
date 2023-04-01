e21d12f87a24d689eda9d8b1824482aa142bf286
Pierre <pierreveissi@gmail.com>
2023-04-01 14:08:28 +0200

    feat: basic router (#4)
    * chore: react router basic
    
    * feat: basic router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import './base.css'
+import { RouterProvider } from 'react-router-dom'
+import router from './routes/router'
 
-export default function App() {
-  return (
-    <div className='App'>
-      <h1>Hello CodeSandbox</h1>
-      <h2 className='text-2xl font-bold underline'>Start editing to see some magic happen!</h2>
-    </div>
-  )
+const App = () => {
+  return <RouterProvider router={router} />
 }
+
+export default App
