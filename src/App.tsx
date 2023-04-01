c7ab2cfb2ce0e16925f02f0adbe003c048826d6a
Pierre <pierreveissi@gmail.com>
2023-04-01 10:21:21 +0000

    chore-eslint-setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import "./styles.css";
+import './styles.css'
 
 export default function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
-  );
+  )
 }
