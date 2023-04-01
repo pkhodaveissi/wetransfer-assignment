00d7b3e59a3570f69617de2e20cf9775a31f1eb0
Pierre <pierreveissi@gmail.com>
2023-04-01 13:17:40 +0200

    chore: react router basic (#3)

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import { RouterProvider } from 'react-router-dom'
+import router from './routes/router'
 
 const rootElement = document.getElementById('root') as HTMLElement
 const root = ReactDOM.createRoot(rootElement)
 
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}  />
   </React.StrictMode>,
 )
