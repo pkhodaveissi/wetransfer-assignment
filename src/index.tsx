b1a496aa6441782fb293d050c9a0e88ef72e0a56
Pierre <pierreveissi@gmail.com>
2023-04-01 10:14:55 +0000

    Update 5 files

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-const rootElement = document.getElementById("root")!;
+const rootElement = document.getElementById("root") as HTMLElement;
 const root = ReactDOM.createRoot(rootElement);
 
 root.render(
