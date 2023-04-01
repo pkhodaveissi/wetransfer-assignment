a9eebd020d5b5d95b480fecb8797be66d87668d4
Pierre <pierreveissi@gmail.com>
2023-04-01 11:16:59 +0000

    chore: react router basic

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+const tailwindConfig = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+export default tailwindConfig
